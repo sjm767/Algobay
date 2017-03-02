bad3cdebd5030b75acbc00a78292e5eeff9c6757
jaeshim <jaeshim@ebay.com>
2017-03-02 17:33:45 +0900

    init

---
+++ Lecture.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#include<algorithm>
+#include<map>
+using namespace std;
+//
+//int main() {
+//	
+//	string str = "Happy %20Joy %20Joy %21";
+//	string text;
+//
+//	for (int i = 0; i < str.length()-1; i++) {
+//
+//		if (str[i] == '%') {
+//			text = str.substr(i+1, 2);
+//
+//			
+//		}
+//	}
+//	
+//	return 0;
+//}
+//
+//string getString(string text) {
+//	string result;
+//	switch (stoi(text))
+//	{
+//	case 20:
+//		result = " ";
+//		break;
+//	case 24:
+//		result = "$";
+//		break;
+//	case 25:
+//		result = "%";
+//		break;
+//	case 28:
+//		result = "(";
+//		break;
+//	case 29:
+//		result = ")";
+//		break;
+//	case 30:
+//	default:
+//		break;
+//	}
+//}
--- Mispell.cpp
+++ Mispell.cpp
@@ -8,30 +8,28 @@ class Texts {
 public:
 	int index;
 	string text;
-
-
 };
-
-int main(string args[]) {
-	int n;
-	int index;
-	string text;
-
-	map<int, string> texts;
-
-	//cin >> n;
-	//cin.clear();
-	//cin.ignore(256, '\n');
-
-	//for (int i = 0; i < n; i++) {		
-	//	getline(cin, text);
-
-	//	texts.insert(pair<int, string>(text[0], text.substr(2)));		
-	//}
-
-	//for (int i = 0; i < texts.size(); i++) {
-	//	cout << texts[i] << endl;
-	//}
-	//
-	//vector<string> texts;
-}
+//
+//int main(string args[]) {
+//	int n;
+//	int index;
+//	string text;
+//
+//	map<int, string> texts;
+//
+//	//cin >> n;
+//	//cin.clear();
+//	//cin.ignore(256, '\n');
+//
+//	//for (int i = 0; i < n; i++) {		
+//	//	getline(cin, text);
+//
+//	//	texts.insert(pair<int, string>(text[0], text.substr(2)));		
+//	//}
+//
+//	//for (int i = 0; i < texts.size(); i++) {
+//	//	cout << texts[i] << endl;
+//	//}
+//	//
+//	//vector<string> texts;
+//}
