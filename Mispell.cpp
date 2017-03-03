868a8868bccac79deeb79cba2adb3d060ad53049
jaeshim <jaeshim@ebay.com>
2017-03-03 13:48:35 +0900

    add new

--- Mispell.cpp
+++ Mispell.cpp
@@ -3,26 +3,26 @@
 #include<vector>
 using namespace std;
 
-
-int main() {
-	int n;
-	
-	int index;
-	string str;
-
-	vector<string> resultList;	
-		
-	cin >> n;
-	for (int i = 0; i < n; i++) {
-		cin >> index;
-		cin >> str;
-
-		resultList.push_back(to_string(i+1).append(" ").append(str.erase(index-1, 1)));
-	}
-
-	for (int i = 0; i < n; i++) {
-		cout << resultList[i] << endl;
-	}	
-
-	return 0;
-}
+//
+//int main() {
+//	int n;
+//	
+//	int index;
+//	string str;
+//
+//	vector<string> resultList;	
+//		
+//	cin >> n;
+//	for (int i = 0; i < n; i++) {
+//		cin >> index;
+//		cin >> str;
+//
+//		resultList.push_back(to_string(i+1).append(" ").append(str.erase(index-1, 1)));
+//	}
+//
+//	for (int i = 0; i < n; i++) {
+//		cout << resultList[i] << endl;
+//	}	
+//
+//	return 0;
+//}
